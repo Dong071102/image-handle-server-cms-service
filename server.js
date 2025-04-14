f4e18958042206b43982b3690be527b1f342b009
Dong071102 <vubadong071102@gmail.com>
2025-04-14 08:52:55 +0700

    change the path

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const app = express();
 const PORT = 15000; // Cổng server
 
 // Đường dẫn tuyệt đối đến thư mục chứa ảnh
-const FACIAL_RECOGNITION_DIR = '/home/dong/NCKH_DATN_2025/CMS_SOURCE/face_recognition_CMS/src/';
-const HUMAN_COUTER_DIR = '/home/dong/NCKH_DATN_2025/CMS_SOURCE/human_couter_CMS/src/';
+const FACIAL_RECOGNITION_DIR = '/home/dong/NCKH_DATN_2025/CMS_SOURCE/face_recognition_CMS/';
+const HUMAN_COUTER_DIR = '/home/dong/NCKH_DATN_2025/CMS_SOURCE/human_couter_CMS/';
+
+// Kiểm tra quyền truy cập thư mục HUMAN_COUTER_DIR
+const fs = require('fs');
+fs.access(HUMAN_COUTER_DIR, fs.constants.R_OK, (err) => {
+    if (err) {
+        console.log(`Không có quyền đọc thư mục: ${HUMAN_COUTER_DIR}`);
+    } else {
+        console.log(`Thư mục ${HUMAN_COUTER_DIR} có quyền đọc.`);
+    }
+});
 
 // Cấu hình Express để phục vụ ảnh từ thư mục evidence_image
 app.use('/facial_recognition', express.static(path.join(FACIAL_RECOGNITION_DIR)));
